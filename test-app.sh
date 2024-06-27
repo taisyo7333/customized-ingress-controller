1df7be864671bffcab455f1c2aff7284da2060aa
Daisuke Inoue <4737631+taisyo7333@users.noreply.github.com>
2024-06-27 19:47:36 +0900

    [test]curl

--- test-app.sh
+++ test-app.sh
@@ -2,10 +2,17 @@
 EXTERNAL_DOMAIN=${1:?}
 export NAMESPACE=test
 export APP_NAME=hello-openshift
+export APP_DOMAIN="${APP_NAME}"."${EXTERNAL_DOMAIN}"
 
 oc new-project "${NAMESPACE}"
 oc label namespace/"${NAMESPACE}" ingress=custom
 oc get namespace "${NAMESPACE}" -o yaml
 
 oc new-app --docker-image=docker.io/openshift/"${APP_NAME}" -n "${NAMESPACE}"
-oc expose svc "${APP_NAME}" --hostname "${APP_NAME}"."${EXTERNAL_DOMAIN}"
+oc expose svc "${APP_NAME}" --hostname "${APP_DOMAIN}"
+
+oc get route -n "${NAMESPACE}"
+
+sleep 10
+
+curl -I "http://${APP_DOMAIN}"
